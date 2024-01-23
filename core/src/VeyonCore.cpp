9ac44abacfa460f7ce204b4673ca891b391be2c6
rudihartono <rudi@radyalabs.com>
2024-01-23 20:38:27 +0700

    change button style

--- core/src/VeyonCore.cpp
+++ core/src/VeyonCore.cpp
@@ -628,7 +628,7 @@ void VeyonCore::initUi()
 
 		app->setStyleSheet(QStringLiteral(
                                "QToolButton:checked {background-color:#88ddff;}"
-                               "QToolButton:hover {background-color:#DAFAFA;}"
+                               "QToolButton:hover {background-color:#e8e8e8;}"
                                "QToolButton {color:#ffffff;}"
 							   "QToolTip {padding:5px; border:0px;}"
 							   ));
